e41024dbde04820e5135fe2714b2ca1ea2f8747e
jtwagirumukiza <jeantwa@gmail.com>
2020-06-11 13:56:22 -0500

    Initial commit

---
+++ src/main/java/miu/edu/cs/cs425/carRentalWebApp/CarRentalWebAppApplication.java
@@ -0,0 +1,13 @@
+package miu.edu.cs.cs425.carRentalWebApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarRentalWebAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarRentalWebAppApplication.class, args);
+	}
+
+}
