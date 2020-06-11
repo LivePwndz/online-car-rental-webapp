4c7475511cc0ffa345059cc90332ba47e3af39b0
jtwagirumukiza <jeantwa@gmail.com>
2020-06-11 16:17:04 -0500

    security removed

--- src/main/java/miu/edu/cs/cs425/carRentalWebApp/service/CarService.java
+++ src/main/java/miu/edu/cs/cs425/carRentalWebApp/service/CarService.java
@@ -1,6 +1,8 @@
 package miu.edu.cs.cs425.carRentalWebApp.service;
 
 import miu.edu.cs.cs425.carRentalWebApp.model.Car;
+import org.springframework.stereotype.Service;
+
 
 public interface CarService {
     public Car registerNewCar(Car car);
